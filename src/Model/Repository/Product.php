e475f7819749f43eea9ae9025312a49d1f4b650f
Galina Is <denis@isyanov.com>
2019-05-01 11:10:26 +0300

    HomeWork 8

--- src/Model/Repository/Product.php
+++ src/Model/Repository/Product.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Model\Repository;
 
 use Model\Entity;
+use Model\Mapper\Product as MapperProduct;
 
 class Product
 {
@@ -64,53 +65,7 @@ class Product
      */
     private function getDataFromSource(array $search = [])
     {
-        $dataSource = [
-            [
-                'id' => 1,
-                'name' => 'PHP',
-                'price' => 15300,
-            ],
-            [
-                'id' => 2,
-                'name' => 'Python',
-                'price' => 20400,
-            ],
-            [
-                'id' => 3,
-                'name' => 'C#',
-                'price' => 30100,
-            ],
-            [
-                'id' => 4,
-                'name' => 'Java',
-                'price' => 30600,
-            ],
-            [
-                'id' => 5,
-                'name' => 'Ruby',
-                'price' => 18600,
-            ],
-            [
-                'id' => 8,
-                'name' => 'Delphi',
-                'price' => 8400,
-            ],
-            [
-                'id' => 9,
-                'name' => 'C++',
-                'price' => 19300,
-            ],
-            [
-                'id' => 10,
-                'name' => 'C',
-                'price' => 12800,
-            ],
-            [
-                'id' => 11,
-                'name' => 'Lua',
-                'price' => 5000,
-            ],
-        ];
+        $dataSource = (new MapperProduct())->selectAll();
 
         if (!count($search)) {
             return $dataSource;
